bcab2be048007f0f07243fb949cad22717c9ae12
Rickey Fehr <ric@rf3.xyz>
2026-06-22 16:33:49 -0700

    Add a simple single pixel demo

--- source/main.c
+++ source/main.c
@@ -1,15 +1,44 @@
+#include "nds/arm9/video.h"
 #include <nds.h>
+#include <gl2d.h>
+
 #include <stdio.h>
+
+#define HALF_WIDTH (SCREEN_WIDTH/2)
+#define HALF_HEIGHT (SCREEN_HEIGHT/2)
+
+void pixels( int frame)
+{
+	glBegin2D();
+
+    auto white = RGB15(-1,-1,-1);
+
+    glPutPixel(frame % SCREEN_WIDTH, HALF_HEIGHT, white);
+
+	glEnd2D();
+}
 
 int main()
 {
     consoleDemoInit();
 
-    printf("Ric is big coolio \n");
+    videoSetMode( MODE_5_3D );
 
-    while (pmMainLoop())
+    consoleDemoInit();
+
+    glScreen2D();
+
+    iprintf("\x1b[1;1HDoin Ya Mom Doin Doin Ya Mom");
+
+    int frame = 0;
+
+    while( pmMainLoop() )
     {
-        swiWaitForVBlank();
+        frame++;
+
+        pixels( frame );
+
+        glFlush(0);
     }
 
     return 0;
